cfddc2e827917cac1eb40997adcd58b97425ff5b
Can Ata <canata09074@gmail.com>
2023-04-05 19:33:19 +0300

    a-a-selector

---
+++ storage/framework/views/173da96d00fed43091de7f83041b37595ba3e3a5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 9 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        body{
+            padding: 50px;
+            background: #f2f5f2;
+        }
+    </style>
+</head>
+<body>
+  
+<div class="container">
+    <?php echo $__env->yieldContent('content'); ?>
+</div>
+   
+</body>
+</html><?php /**PATH C:\Users\user\Desktop\Can2_Stream\fileapp38\Laravel-9-CRUD\resources\views/products/layout.blade.php ENDPATH**/ ?>
